7eeca832e5f7e9547d4b33c377997c25e89c22d9
bhaveshlande <bhavesh.baliram-lande@capgemini.com>
2023-09-12 12:14:47 +0530

    integrated redux shopmate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components";
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
